9709a920bf29cbc715c8f9f08bd0ec477910cac5
Vyacheslav-KL <slavkin.131@gmail.com>
2021-11-28 18:51:07 +1000

    Lesson 10. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "mainapp.context_processors.basket",
                 "social_django.context_processors.backends",
-                "social_django.context_processors.login_redirect",                
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
